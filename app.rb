2e21bc1feb42c6aec30760cc518245dd4823321d
syon <syon.xiii@gmail.com>
2014-03-23 17:27:38 +0900

    Haml Bootstrap template

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
 
+ENV['RACK_ENV'] = "development" if ENV['RACK_ENV'] == nil
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
 ActiveRecord::Base.establish_connection(ENV['RACK_ENV'])
 
@@ -24,4 +25,8 @@ end
 
 get '/initializr' do
   erb :initializr
+end
+
+get '/words/bless' do
+  haml :"words/bless"
 end
