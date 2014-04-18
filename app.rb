38b140bbcc53aafaca25d6edcdcbdc996cf3b846
syon <syon.xiii@gmail.com>
2014-04-19 01:05:38 +0900

    puts ENV['RACK_ENV']

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require "sinatra/content_for"
 require 'active_record'
 
 ENV['RACK_ENV'] = "development" if ENV['RACK_ENV'].nil?
+puts "ENV['RACK_ENV']: #{ENV['RACK_ENV']}"
 ActiveRecord::Base.configurations = YAML.load(ERB.new(File.read('config/database.yml')).result)
 ActiveRecord::Base.establish_connection(ENV['RACK_ENV'])
 use ActiveRecord::ConnectionAdapters::ConnectionManagement # for Connection Error
