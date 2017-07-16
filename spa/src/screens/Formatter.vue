4dad421b92e3e8722dee0c98c9ff2b67a33a69b7
syon <syon.xiii@gmail.com>
2017-07-16 15:50:29 +0900

    :sparkles: formatter

---
+++ spa/src/screens/Formatter.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.screen
+  .container
+    h2 Formatter index
+
+    ul
+      li
+        a(href="./html/") HTML
+      li
+        a(href="./css/") CSS
+      li
+        a(href="./sql/") SQL
+
+  global-footer
+</template>
+
+<script>
+import GlobalFooter from '@/components/GlobalFooter';
+
+export default {
+  name: 'hello',
+  components: {
+    GlobalFooter,
+  },
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+</style>
