29246e761233ca8c85a9fb66f4ab0a6578c5a620
syon <syon.xiii@gmail.com>
2017-07-16 15:01:45 +0900

    :package: Stylus

--- spa/src/components/Hello.vue
+++ spa/src/components/Hello.vue
@@ -38,22 +38,18 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="stylus" scoped>
+h1, h2
+  font-weight normal
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+ul
+  list-style-type none
+  padding 0
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+li
+  display inline-block
+  margin 0 10px
 
-a {
-  color: #42b983;
-}
+a
+  color #42b983
 </style>
