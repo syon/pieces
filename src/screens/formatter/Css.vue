ed3a1cec0049f9d2407f155033403f5a1cfd590f
syon <syon.xiii@gmail.com>
2017-07-17 15:47:17 +0900

    :sparkles: CSS Formatter

--- src/screens/formatter/Css.vue
+++ src/screens/formatter/Css.vue
@@ -3,19 +3,62 @@
   .container
     h2 CSS Formatter
 
+    textarea#src.block(v-model="src")
+
+    .centering
+      button#format.button-primary(@click="doFormat") Format
+
+    pre#dest {{ dest }}
+
   global-footer
 </template>
 
 <script>
 import GlobalFooter from '@/components/GlobalFooter';
+import jsBeautify from 'js-beautify';
 
 export default {
   name: 'css',
   components: {
     GlobalFooter,
   },
+  data() {
+    return {
+      src: 'body{color:red;}',
+      dest: '',
+    };
+  },
+  methods: {
+    doFormat() {
+      this.dest = jsBeautify.css(this.src);
+    },
+  },
 };
 </script>
 
 <style lang="stylus" scoped>
+.block {
+  display: block;
+  width: 100%;
+}
+
+#src, #dest {
+  font-family: Consolas, "Liberation Mono", Menlo, Courier, monospace;
+  font-size: 12px;
+}
+
+#src {
+  height: 150px;
+}
+
+#dest {
+  height: 300px;
+  padding: 5px;
+  background-color: #f5f5f5;
+  overflow-x: scroll;
+}
+
+.centering {
+  text-align: center;
+}
 </style>
