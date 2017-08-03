96e53c126e05e3822ed90b388d39b488ac470241
syon <syon.xiii@gmail.com>
2017-08-03 19:19:32 +0900

    :art: top page

---
+++ src/components/MenuFooter.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+footer
+  .container
+    hr
+    .meta
+      a(href="https://github.com/syon" class="author").
+        <img src="../assets/syon.png" alt="syon" target="_blank"><span>syon</span></a>
+      
+      .repo
+        a(href="https://github.com/syon/pieces" target="_blank")
+          span(uk-icon="icon:github")
+        a(href="https://app.wercker.com/project/byKey/a85109629f315de1a3cf4e0521775121")
+          img(alt="Wercker status" src="https://app.wercker.com/status/a85109629f315de1a3cf4e0521775121/s/master")
+</template>
+
+<script>
+export default {
+  name: 'menu-footer',
+};
+</script>
+
+<style lang="stylus" scoped>
+footer
+  hr
+    margin 0
+    border 0
+    border-top 1px solid #eee
+  .meta
+    display flex
+    align-items center
+    justify-content space-between
+    padding 8px 16px
+    height 50px
+    font-size 12px
+    color #7e888b
+    .author
+      display flex
+      align-items center
+      text-decoration none
+      color inherit
+      img
+        width 30px
+        height 30px
+        margin-right .4rem
+        border-radius 100%
+    .repo
+      display flex
+      a
+        margin-left 15px
+        color #7e888b
+        text-decoration none
+</style>
--- src/screens/Hello.vue
+++ src/screens/Hello.vue
@@ -1,58 +1,54 @@
 <template lang="pug">
 .screen
-  .container
-    h1 Pieces
-    ul
-      li
-        router-link(to="/chartjs/") Chart.js
-    ul
-      li
-        router-link(to="/formatter/html/") HTML Formatter
-      li
-        router-link(to="/formatter/css/") CSS Formatter
-      li
-        router-link(to="/formatter/sql/") SQL Formatter
-    ul
-      li
-        router-link(to="/tips/checkbox/") Checkbox
-    ul
-      li
-        router-link(to="/words/adage/") 心に響く言葉
-      li
-        router-link(to="/words/bless/") Words of Blessings
-    ul
-      li
-        router-link(to="/url-encode-decode/") URLエンコード
-      li
-        router-link(to="/misc/pixel-color/") PixelColor
-      li
-        router-link(to="/misc/vscode-vsix/") VSCode - VSIX Downloader
-  global-footer
+  .tm-appbar.uk-navbar-container
+    .uk-container
+      nav.uk-navbar
+        div.uk-navbar-left
+          ul.uk-navbar-nav
+            a(href="") Pieces
+
+  .tm-sidebar-left
+    .tm-menu
+      ul
+        li
+          router-link(to="/chartjs/") Chart.js
+      ul
+        li
+          router-link(to="/formatter/html/") HTML Formatter
+        li
+          router-link(to="/formatter/css/") CSS Formatter
+        li
+          router-link(to="/formatter/sql/") SQL Formatter
+      ul
+        li
+          router-link(to="/tips/checkbox/") Checkbox
+      ul
+        li
+          router-link(to="/words/adage/") 心に響く言葉
+        li
+          router-link(to="/words/bless/") Words of Blessings
+      ul
+        li
+          router-link(to="/url-encode-decode/") URLエンコード
+        li
+          router-link(to="/misc/pixel-color/") PixelColor
+        li
+          router-link(to="/misc/vscode-vsix/") VSCode - VSIX Downloader
+
+    .tm-author
+      menu-footer
 </template>
 
 <script>
-import GlobalFooter from '@/components/GlobalFooter';
+import MenuFooter from '@/components/MenuFooter';
 
 export default {
   name: 'hello',
   components: {
-    GlobalFooter,
+    MenuFooter,
   },
 };
 </script>
 
 <style lang="stylus" scoped>
-h1, h2
-  font-weight normal
-
-ul
-  list-style-type none
-  padding 0
-
-li
-  display inline-block
-  margin 0 10px
-
-a
-  color #42b983
 </style>
