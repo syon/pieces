e5f7e896d76aad6560ddec8d0703c9ad630cb09d
syon <syon.xiii@gmail.com>
2017-08-03 21:18:07 +0900

    :art: Sidebar styling

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,31 +1,32 @@
 <template lang="pug">
 .tm-sidebar-left
   .tm-menu
-    ul
-      li
-        router-link(to="/chartjs/") Chart.js
-    ul
+    ul.uk-nav.uk-nav-default
+
+      li.uk-nav-header Web Tools
       li
         router-link(to="/formatter/html/") HTML Formatter
       li
         router-link(to="/formatter/css/") CSS Formatter
       li
         router-link(to="/formatter/sql/") SQL Formatter
-    ul
-      li
-        router-link(to="/tips/checkbox/") Checkbox
-    ul
-      li
-        router-link(to="/words/adage/") 心に響く言葉
-      li
-        router-link(to="/words/bless/") Words of Blessings
-    ul
       li
         router-link(to="/url-encode-decode/") URLエンコード
+
+      li.uk-nav-header Code
+      li
+        router-link(to="/chartjs/") Chart.js
+      li
+        router-link(to="/tips/checkbox/") Checkbox
       li
         router-link(to="/misc/pixel-color/") PixelColor
       li
         router-link(to="/misc/vscode-vsix/") VSCode - VSIX Downloader
+
+      li
+        router-link(to="/words/adage/") 心に響く言葉
+      li
+        router-link(to="/words/bless/") Words of Blessings
 
   .tm-author
     menu-footer
@@ -55,6 +56,23 @@ export default {
     flex 1
     box-sizing border-box
     width 240px !important
-    padding 40px 16px 0
+    padding 40px 15px 0 40px
     overflow auto
+
+  .uk-nav-header
+    margin-bottom 3px
+    padding 8px 0
+    font-size 12px
+    border-bottom 1px solid #e5e5e5
+
+  a.router-link-active
+    position relative
+    color #222
+    &:before
+      content ""
+      position absolute
+      top 15px
+      left -25px
+      width 15px
+      border-top 1px solid #1e87f0
 </style>
