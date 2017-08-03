7ca058ff0990c70645f81073c23bfa1f37c4e998
syon <syon.xiii@gmail.com>
2017-08-03 19:18:57 +0900

    :package: install UIKit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+import 'uikit/dist/css/uikit.css';
+import './assets/uikit.theme.stylus';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+UIkit.use(Icons);
 
 /* eslint-disable no-new */
 new Vue({
