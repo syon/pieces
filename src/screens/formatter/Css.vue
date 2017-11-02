b5460ca0a6bf9dec0c733987efbf56c28d6123ed
syon <syon.xiii@gmail.com>
2017-11-02 21:48:06 +0900

    :art: add options

--- src/screens/formatter/Css.vue
+++ src/screens/formatter/Css.vue
@@ -3,17 +3,55 @@
   .container
     h2 CSS Formatter
 
-    textarea#src.uk-textarea.block(v-model="src")
+  .container.uk-flex-1(uk-grid)
+    .uk-width-3-4.uk-flex.uk-flex-column
+      textarea#src.uk-textarea.block(v-model="src")
 
-    .uk-margin.centering
-      button#format.uk-button.uk-button-primary(@click="doFormat") Format
+      .uk-margin.centering
+        button#format.uk-button.uk-button-primary(@click="doFormat") Format
 
-    .uk-position-relative
+      .uk-position-relative
 
-      .uk-card.uk-card-default.uk-card-small.uk-card-body.uk-margin
-        pre#dest {{ dest }}
+        .uk-card.uk-card-default.uk-card-small.uk-card-body.uk-margin
+          pre#dest {{ dest }}
 
-      copy-to-clipboard(target="#dest")
+        copy-to-clipboard(target="#dest")
+
+    .uk-width-1-4
+      .options-header Options
+      form.uk-form-stacked
+        .uk-margin
+          .uk-form-label インデント
+          .uk-grid-small.uk-child-width-auto.uk-grid.uk-child-width-expand
+            label
+              input.uk-radio(type="radio" v-model="indent_width" value="2" name="indent_width")
+              span.uk-margin-small-left 2
+            label
+              input.uk-radio(type="radio" v-model="indent_width" value="4" name="indent_width")
+              span.uk-margin-small-left 4
+            label
+              input.uk-radio(type="radio" v-model="indent_width" value="tab" name="indent_width")
+              span.uk-margin-small-left TAB
+        .uk-margin
+          .uk-form-label 改行コード
+          .uk-grid-small.uk-child-width-auto.uk-grid.uk-child-width-expand
+            label
+              input.uk-radio(type="radio" v-model="eol" value="\\r\\n" name="eol")
+              span.uk-margin-small-left CRLF
+            label
+              input.uk-radio(type="radio" v-model="eol" value="\\n" name="eol")
+              span.uk-margin-small-left LF
+        .uk-margin
+          .uk-grid-small.uk-child-width-auto.uk-grid
+            label
+              input.uk-checkbox(type="checkbox" v-model="preserve_newlines" name="preserve_newlines")
+              span.uk-margin-small-left 改行を維持する
+      .options-header Links
+      ul
+        li
+          a(href="https://github.com/beautify-web/js-beautify")
+            | js-beautify
+      adsense(ad-client="ca-pub-2136746860059501" ad-slot="9805871014" ad-style="display:block" ad-format="auto")
 </template>
 
 <script>
@@ -29,11 +67,20 @@ export default {
     return {
       src: 'body{color:red;}',
       dest: '',
+      indent_width: 2,
+      preserve_newlines: true,
+      eol: '\\r\\n',
     };
   },
   methods: {
     doFormat() {
-      this.dest = jsBeautify.css(this.src);
+      const opts = {
+        indent_size: this.indent_width,
+        indent_with_tabs: this.indent_width === 'tab',
+        preserve_newlines: this.preserve_newlines,
+        eol: this.eol,
+      };
+      this.dest = jsBeautify.css(this.src, opts);
     },
   },
 };
@@ -64,4 +111,11 @@ export default {
 .centering {
   text-align: center;
 }
+
+.options-header
+  font-size 14px
+  margin-bottom 15px
+  padding 8px 0
+  font-size 12px
+  border-bottom 1px solid #e5e5e5
 </style>
